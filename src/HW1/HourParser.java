27497427b74007fde94847ab29fdb882e329938f
Ian <Ian@IansMacBookPro.local>
2016-01-20 20:53:01 -0600

    Start of the program.

--- src/HW1/HourParser.java
+++ src/HW1/HourParser.java
@@ -1,7 +1,25 @@
 package HW1;
 
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class HourParser {
-
-
-
+	
+	public static void main(String[] args) {
+		int hours = 0;
+		
+		String input = "";
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter the amount of hours to average out: ");
+		
+		input = scan.next();
+		scan.close();
+		
+		hours = Integer.parseInt(input);
+		
+		
+	}
 }
