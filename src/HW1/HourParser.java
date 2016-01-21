f3ca6182d86d9a21758ca7e5951fb64b9cdecdc4
Ian Lim <limik@lopers.unk.edu>
2016-01-20 22:35:59 -0600

    Created the parsed segments.

--- src/HW1/HourParser.java
+++ src/HW1/HourParser.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
  * As user for the number of hours. (TeamName)
  */
 public class HourParser {
+	private final int SEGLENGTH = 10;
 
     private String fileName;
     private int hours;
@@ -33,38 +34,49 @@ public class HourParser {
         }
 
         SimpleDateFormat dateFormatter = new SimpleDateFormat("EEE MMM dd kk:mm:ss z yyyy", Locale.ENGLISH);
-        //int segmentCounter = 0; //So we only get one segment to test with.
-        while(fileScanner.hasNextLine())
-        {
-            try {
-                //Testing code from Homework specs.
-                String date = fileScanner.nextLine();
-                Date formattedDate = dateFormatter.parse(date);
-                System.out.println(date + "\n" + formattedDate);
-                System.exit(0);
+        int segmentCounter = 0; //So we only get one segment to test with.
+        
+        try {
+    		//Creates array depending on how many hours the user enters.
+    		String[][] segments = new String[hours][SEGLENGTH];
+    		segments = getSegments(fileScanner,segments);
 
-                //System.out.println(fileScanner.nextLine());
-                //segmentCounter++;
-                //if (segmentCounter == 10)
-                //    break;
-            } catch(ParseException e) {
-                System.out.println("Error in parsing the line.");
-            }
-        }
+          } catch(Exception e) { //Change Exception
+              System.out.println("Error in parsing the line.");
+          }
     }
 
     /**
      * This method should get the segments of info depending on the hours.
      */
-    public void getSegments() {
-
+    public String[][] getSegments(Scanner scan, String[][] seg) {
+    	int hourCount = 0;
+    	int lineCount = 0;
+    	
+    	while(scan.hasNextLine()) {
+    		seg[hourCount][lineCount] = scan.nextLine();
+    		
+    		System.out.println(seg[hourCount][lineCount]); //Prints current line
+    		
+    		if(hourCount == (hours-1) && lineCount == (SEGLENGTH-1)) {
+    			break;
+    		}else if(lineCount == (SEGLENGTH-1)) {
+    			lineCount = 0;
+    			hourCount++;
+    			System.out.println("\n");
+    		} else {
+    			lineCount++;
+    		}
+    	}
+    	
+    	return seg;
     }
 
     /**
      * This method gets the throughput from each segment of data.
      */
     public void getThroughput() {
-
+    	
     }
 
     /**
