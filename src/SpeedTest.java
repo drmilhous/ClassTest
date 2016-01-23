34f92a5079e3d4f1c0013fd9cf68e0e4e0677208
Burritocow <fertigkc@lopers.unk.edu>
2016-01-23 00:31:48 -0600

    Dan look at last function of SpeedTest

--- src/SpeedTest.java
+++ src/SpeedTest.java
@@ -13,23 +13,33 @@ public class SpeedTest {
     static int DAY_WIDTH = 86400;
 
     public static void main(String args[]) {
+
+       /*
         Scanner scanin = new Scanner(System.in);
-        System.out.println("This program will show the speed test results/nHow many days would you like to check?\n");
+        System.out.println("This program will show the speed test results.\nHow many days would you like to check?\n");
 
         int days2Check = scanin.nextInt();
-
+        */
         File file = new File("speed.txt");
 
         Parser parser = new Parser(file);
         Test[] tests = parser.readInFile();
 
         Date startDate = tests[0].getDateTime();
-        startDate.setHours(0);
-        startDate.setMinutes(0);
+
         startDate.setSeconds(0);
+        if(startDate.getMinutes() < 30) {
+            startDate.setMinutes(0);
+        }else{
+            startDate.setMinutes(30);
+        }
+
+
+        long lastInterval = dateToUnix(startDate);
+        startDate.setMinutes(0);
+        startDate.setHours(0);
 
         long lastDay = dateToUnix(startDate);
-        long lastInterval = dateToUnix(startDate);
 
         ArrayList<Interval> intervals = new ArrayList<Interval>();
         ArrayList<Test> tests4Interval = new ArrayList<Test>();
@@ -44,6 +54,9 @@ public class SpeedTest {
                 tests4Interval.add(tests[i]);
                 lastInterval = dateToUnix(tests[i].getDateTime());
             }
+            System.out.println("interval: " + intervals.size());
+            System.out.println("test: " + tests[i].getDateTime());
+            System.out.println("i: " + i + "\n");
         }
 
         if(!tests4Interval.isEmpty()) {
@@ -72,9 +85,9 @@ public class SpeedTest {
             days.add(new Day(daily, daily.get(0).getStartDateTime()));
         }
 
-        /* Dan you can call the print.out function from here if you want.
+       //printResults(days2Check, days);
 
-         */
+
     }
 
     public static long dateToUnix(Date date){
@@ -87,4 +100,15 @@ public class SpeedTest {
         Date date = new Date(ut * 1000);
         return date;
     }
+
+    public static void printResults(int days, ArrayList<Day> dayList) {
+        for(int i=0; i<dayList.size(); i++) {
+            for(int j=0; j<dayList.get(i).getIntervals().size(); j++) {
+                System.out.println(dayList.get(i).getIntervals().get(i).getStartDateTime());
+                System.out.println("Download: " + dayList.get(i).getIntervals().get(i).getAvgDown() + " mbps");
+                System.out.println("Upload: " + dayList.get(i).getIntervals().get(i).getAvgUp() + " mbps");
+            }
+        }
+    }
+
 }
