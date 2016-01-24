3357284f7f9a096cb6a7a5a07bb87a76b2b5d70e
harshiz <harshbargedl@lopers.unk.edu>
2016-01-23 18:15:43 -0600

    CSVoutput and speedTest modified. Working.

--- src/CSVOutput.java
+++ src/CSVOutput.java
@@ -1,31 +1,60 @@
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by Dan Harshbarger on 1/22/2016.
  */
 public class CSVOutput {
+    static int DAY_WIDTH = 86400;
 
-    public static void generateCSV(int daysBack, ArrayList<Day> days) {
+    public static void generateCSV(int days, ArrayList<Interval> inv) {
+
+        Date curDate = new Date();
+        curDate.setHours(23);
+        curDate.setMinutes(59);
+        curDate.setSeconds(59);
+
+        long to = dateToUnix(curDate);
+        long from = to - (days * DAY_WIDTH) + 1;
+        long tempDate = 0;
+
+        DecimalFormat df = new DecimalFormat("#.00");
+        int counter = 0;
 
         try{
             FileWriter writer = new FileWriter("Average.csv");
 
-            for(int i = 0; i < days.size();i++){
-
-                for(int j = 0; j < days.get(i).getIntervals().size();j++){
-
-                    //writer.append(days.get(i).getIntervals().get(j).getStartDateTime());
-                    writer.append("Download: " + days.get(i).getIntervals().get(j).getAvgDown() + " mbps");
-                    writer.append("Upload: " + days.get(i).getIntervals().get(j).getAvgUp() + " mbps");
+            for(int i = inv.size()-1; i >= 0; i--){
+                    tempDate = inv.get(i).getStartDateTime();
+                if(tempDate <= to && tempDate >= from) {
+                    writer.append(unixToDate(tempDate).toString() + "\t");
+                    writer.append("Average Download: " + df.format(inv.get(i).getAvgDown()) + " mbps\t");
+                    writer.append("Average Upload: " + df.format(inv.get(i).getAvgUp()) + " mbps\n");
+                    counter ++;
                 }
             }
 
+            if(counter == 0){
+                writer.append("No records found for entered time-frame");
+            }
             writer.close();
 
         }catch(IOException e){
             System.out.println("Didn't Work");
         }
+    }
+
+    public static Date unixToDate(long ut) {
+
+        Date date = new Date(ut * 1000);
+        return date;
+    }
+
+    public static long dateToUnix(Date date){
+        long unixTime = date.getTime()/1000;
+        return unixTime;
     }
 }
