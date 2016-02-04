20d8013e26790066bfafd2f20c25eb4f0f137c07
Sujan Shrestha <shresthas8@lopers.unk.edu>
2016-02-04 10:01:32 -0600

    Parse redone

--- hw2/Parse.java
+++ hw2/Parse.java
@@ -1,91 +1,84 @@
 package hw2;
 
+
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Parse {
-
-	private LinkedList <LogEntry> entries;
-	private Date time;
-	
-
-	public Parse(String path)
+public class Parse
 	{
-		File file = new File(path);
-		Scanner scan = null;
-		try {
-			scan = new Scanner(file);
-		} catch (Exception e) {
+		private LinkedList<LogEntry> entries;
 
-			e.printStackTrace();
-		}
-
-		if (scan != null)
-		{
-			SimpleDateFormat dateFormat = new SimpleDateFormat("EEE MMM dd kk:mm:ss z yyyy", Locale.ENGLISH);
-			String scn = "";
-
-			while(scan.hasNext())
+		public Parse(String path)
 			{
-				 Float download = 0f;
-				 Float upload = 0f;
-				String yourLine = scan.nextLine(); //Stores a single line in variable
-
-				if(yourLine.startsWith("Mon") || yourLine.startsWith("Tue") || 
-						yourLine.startsWith("Wed") || yourLine.startsWith("Thu") || 
-						yourLine.startsWith("Fri") || yourLine.startsWith("Sat") ||
-						yourLine.startsWith("Sun"))
-				{
-					try
+				setEntries(new LinkedList<LogEntry>());
+				float Download = 0;
+				float UploadXX = 0;
+				File f = new File(path);
+				Scanner s = null;
+				try
 					{
-						time=(dateFormat.parse(yourLine)); //Adds date and time to time ArrayList
-						;
+						s = new Scanner(f);
 					}
-					catch(Exception e)
+				catch (FileNotFoundException e)
 					{
 						e.printStackTrace();
 					}
-				}
+				if (s != null)
+					{
+						String line = s.nextLine();
+						Date now = new Date();
+						now.setTime(now.getTime() - 60 * 60 * 24 * 1000);
+						Date d = new Date();
 
-				//Adds Download value to download ArrayList
-				if(yourLine.startsWith("Download: "))
-				{
-					String[] splitData=yourLine.split(" ");
-					download = Float.parseFloat(splitData[1]) ;
-					
-					//Skips two line to get to the upload
-					yourLine = scan.nextLine();
-					yourLine = scan.nextLine();
-				}
-				
-				
-				
-				//Adds Upload value to download ArrayList
-				if(yourLine.startsWith("Upload: "))
-				{
-					String[] splitData=yourLine.split(" ");
-					upload = Float.parseFloat(splitData[1]); 
-//					System.out.println(time);
-//					System.out.println(download);
-//					System.out.println(upload);
-					getEntries().add(new LogEntry( download, upload, time));
-				}
-				
+						while (s.hasNextLine())
+							{
+								int space1 = line.indexOf(" ");
+								int space2 = line.indexOf(" ", space1 + 1);
+								if (line.contains("Upload:"))
+									{
+										UploadXX = Float.parseFloat(line.substring(space1, space2));
+										// System.out.println(Download + "," + Upload + "," + d.getHours() + ":" + d.getMinutes());
+										getEntries().add(new LogEntry(Download, UploadXX, d));
+
+									}
+								else if (line.contains("Download"))
+									{
+										Download = Float.parseFloat(line.substring(space1, space2));
+
+									}
+								else if (line.contains("CST"))
+									{
+										SimpleDateFormat sdfmt1 = new SimpleDateFormat("MM dd HH:mm:ss yyyy");
+										sdfmt1 = new SimpleDateFormat("EEE MMM dd kk:mm:ss z yyyy", Locale.ENGLISH);
+										try
+											{
+												d = sdfmt1.parse(line);
+											}
+										catch (ParseException e)
+											{
+												e.printStackTrace();
+											}
+									}
+
+								line = s.nextLine();
+							}
+						s.close();
+					}
 			}
 
-			scan.close();
-		}
-	}
+		public LinkedList<LogEntry> getEntries()
+			{
+				return entries;
+			}
 
-	public LinkedList<LogEntry> getEntries() {
-		return entries;
+		public void setEntries(LinkedList<LogEntry> entries)
+			{
+				this.entries = entries;
+			}
 	}
-	public void setEntries(LinkedList<LogEntry> entries) {
-		this.entries = entries;
-	}
-
-}
